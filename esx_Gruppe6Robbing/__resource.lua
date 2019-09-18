0c53a901308b1d20967169910deb3de9caf2dde7
Mikou27 <49500798+Mikou27@users.noreply.github.com>
2019-09-18 10:41:49 +0200

    Add files via upload

---
+++ esx_Gruppe6Robbing/__resource.lua
@@ -0,0 +1,17 @@
+--[[Original script [Gruppe6Robbing] by NENE(mikou)]]--
+---https://forum.fivem.net/t/release-vrp-gruppe6robbing-gruppe6-armored-truck-robbing/290234
+
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9 '
+
+version '1.0.0'
+
+client_scripts {
+'@es_extended/locale.lua',
+'server/main.lua'
+}
+
+
+server_scripts {
+'@es_extended/locale.lua',
+'client/main.lua'
+}
