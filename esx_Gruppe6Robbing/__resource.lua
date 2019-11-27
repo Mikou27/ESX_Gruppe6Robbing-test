32924e90be23f68b71475b07607b191a81845474
BronxDre <33867736+BronxDre@users.noreply.github.com>
2019-11-27 08:03:13 -0500

    Update __resource.lua

--- esx_Gruppe6Robbing/__resource.lua
+++ esx_Gruppe6Robbing/__resource.lua
@@ -7,11 +7,13 @@ version '1.0.0'
 
 client_scripts {
 '@es_extended/locale.lua',
-'client/main.lua'
+'client/main.lua',
+'config.lua'
 }
 
 
 server_scripts {
 '@es_extended/locale.lua',
-'server/main.lua'
+'server/main.lua',
+'config.lua'
 }
